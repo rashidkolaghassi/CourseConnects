141820e5528c6b41ca2b50b946d40a8af7248425
rashidkolaghassi <rashidkolaghassi@gmail.com>
2023-04-17 21:15:56 -0400

    Delete requestTry.py

--- requestTry.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-BASE = "http://192.168.1.221:8000"
-response=requests.post(BASE+"/api/login" ,json=
-                              {"username":"rashid01",
-                               "password":"123"
-                        })
-print(response.json())
